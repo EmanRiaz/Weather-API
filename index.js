7075073ad5311c660a851b508d94f8798bd86591
EmanRiaz <imanriaz81@gmail.com>
2024-08-07 21:15:51 +0500

    Fixed issues with wave animations and updated server script

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 const http = require("http");
 const fs = require("fs");
-const axios = require("axios");
+const requests = require("requests");
 
 const homeFile = fs.readFileSync("home.html", "utf-8");
 
-const replaceVal = (tempVal, weatherData) => {
-    let temperature = tempVal.replace("{%tempval%}", weatherData.main.temp);
-    temperature = temperature.replace("{%tempmin%}", weatherData.main.temp_min);
-    temperature = temperature.replace("{%tempmax%}", weatherData.main.temp_max);
-    temperature = temperature.replace("{%location%}", weatherData.name);
-    temperature = temperature.replace("{%tempcountry%}", weatherData.sys.country);
+const replaceVal = (tempVal, orgVal) => {
+    let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+    temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+    temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+    temperature = temperature.replace("{%location%}", orgVal.name);
+    temperature = temperature.replace("{%tempcountry%}", orgVal.sys.country);
+    temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
+
     return temperature;
 };
 
 const server = http.createServer((req, res) => {
     if (req.url === "/") {
-        axios.get("http://api.openweathermap.org/data/2.5/weather?q=Lahore&appid=4600bf6159b019d27fa51c88a40b86c0&units=metric")
-            .then(response => {
-                const weatherData = response.data;
-                const realTimeData = replaceVal(homeFile, weatherData);
+        requests("http://api.openweathermap.org/data/2.5/weather?q=Lahore&appid=4600bf6159b019d27fa51c88a40b86c0&units=metric")
+            .on("data", (chunk) => {
+                const objdata = JSON.parse(chunk);
+                const realTimeData = replaceVal(homeFile, objdata);
                 res.write(realTimeData);
-                res.end();
             })
-            .catch(error => {
-                console.log("Error fetching data from API:", error);
+            .on("end", (err) => {
+                if (err) {
+                    console.log("Connection closed due to errors", err);
+                }
                 res.end();
             });
+    } else {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.write("404 Not Found");
+        res.end();
     }
 });
 
